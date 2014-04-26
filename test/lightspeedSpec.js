64ed4636ff269ba4259cc4653b0835e67525c563
Scott Trinh <scottyparade@gmail.com>
2014-04-25 22:36:44 -0400

    Make method call based on method string passed in.

--- test/lightspeedSpec.js
+++ test/lightspeedSpec.js
@@ -3,10 +3,17 @@ var ls = require("../lib/lightspeed.js");
 var args = ["Orders",{"limit":"1"}];
 
 describe("LightSpeed", function() {
+
+  it("should load successfully", function(){
+    expect(ls).to.not.be.null;
+  });
+
   it("should use environment variables for secrets", function() {
     expect(process.env.LS_API_KEY).to.exist;
     expect(process.env.LS_ACCOUNT).to.exist;
   });
+
   describe(".get()", function() {
+    
   });
 });
