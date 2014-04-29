69b6e9427b60e6bd2f13773b48eedd5a3985f315
Scott Trinh <scott@scotttrinh.com>
2014-04-29 09:29:48 -0400

    Made load_relations="all" default behavior for LightSpeed.

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ var eshake = require("./lib/earthshaking");
 var port = 3000;
 
 http.createServer(function (request, response) {
-  eshake.orders.get('',{"params":{}},
+  eshake.orders.get('',{"params":{"limit":"50","complete":"false"}},
                     function(res) {
                       body = JSON.stringify(res);
+                      response.setHeader("Access-Control-Allow-Origin","*");
                       response.writeHead(200, { "Content-Type":"application/json" });
                       response.write(body);
                       response.end();
