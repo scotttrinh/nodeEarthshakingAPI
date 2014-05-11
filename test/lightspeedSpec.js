1fa605b7c571653ad11829d27588743a06688d56
Scott Trinh <scott@scotttrinh.com>
2014-05-11 00:52:22 -0400

    Added first bit of Hypermedia to LightSpeed. Array wrapper.
    Added unit tests to add href links to collections and the instances in them.
    Array wrapper needs to be refactored for instance endpoints. No arrays need
    to be added to them.

--- test/lightspeedSpec.js
+++ test/lightspeedSpec.js
@@ -2,6 +2,8 @@ var expect = require("chai").expect;
 var ls = require("../lib/lightspeed.js");
 var _ = require("lodash");
 
+var REST_ROOT = 'https://api.merchantos.com/API/Account/' +
+                process.env.LS_ACCOUNT + '/';
 describe("LightSpeed", function() {
 
   it("should load successfully", function(){
@@ -13,22 +15,50 @@ describe("LightSpeed", function() {
     expect(process.env.LS_ACCOUNT).to.exist;
   });
 
+  describe("._addHate()", function(){
+    it("should take an object and add an href to itself", function() {
+      var url = 'http://this.url/';
+      var resource = {};
+      var resourceWithHref = ls._addHate(url, resource);
+      expect(resourceWithHref.href).to.equal(url);
+    });
+  });
+
   describe(".createCall()", function() {
-    var lsOrder;
+    var lsResponse;
+    var testEntity = 'Employee';
+    var resourceID = (function(str) { return str.substr(0,1).toLowerCase() + str.substr(1); })(testEntity) + 'ID';
 
     before(function (done) {
       ls.createCall('get',
-                    'Order',
+                    testEntity,
                     {params:{limit:1,load_relations:"all"}},
                     function(res){
-                      lsOrder = res;
+                      lsResponse = res;
+                      console.log(lsResponse);
                       done();
                     });
     });
 
-    it("should return a non-empty object response", function() {
-      expect(_.isEmpty(lsOrder)).to.be.false;
-      expect(_.isObject(lsOrder)).to.be.true;
+    it("should return a non empty value", function() {
+      expect(_.isEmpty(lsResponse)).to.be.false;
+    });
+    it("should return an object", function() {
+      expect(_.isObject(lsResponse)).to.be.true;
+    });
+    it("should return correct number of resources", function() {
+      if (lsResponse["@attributes"].count > 0) {
+        expect(lsResponse[testEntity]).to.have.length(1);
+      } else {
+        expect(1).to.equal(1);
+      }
+    });
+    it("should contain a universal identifier href", function() {
+      expect(lsResponse.href).to.exist;
+    });
+    it("should go to the resource and add hrefs to each instance", function() {
+      var url = REST_ROOT + testEntity + '/' + lsResponse[testEntity][0][resourceID] + '.json';
+      expect(lsResponse[testEntity][0].href).to.equal(url);
     });
 
   });
